a8e680e007dd2f3ef6812aeb8699bfe0641f35ab
Anjolaoluwa Daniel Akindipe <anjyakindipe@gmail.com>
2022-11-18 00:04:26 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using CliWrap;
+using CliWrap.Buffered;
+
+namespace GitCloneAutomator;
+
+internal static class Program
+{
+    public static async Task Main()
+    {
+        await Start();
+    }
+
+    private static async Task Start()
+    {
+        Console.WriteLine("WELCOME TO GITCLONER");
+
+        /// Get directory from user
+        String? directory = null;
+        {
+            InputValidation(ref directory, "Please input the directory you want the repository to be cloned into: ");
+        }
+
+        // Get repository link from user
+        String? repo = null;
+        {
+            InputValidation(ref repo, "Please provide the repository link: ");
+        }
+
+        // get current directory and change it to user directory
+
+        if (directory != null) Directory.SetCurrentDirectory(directory);
+
+        // run command to clone repository given by into request directory
+        var gitResult = await Cli.Wrap("git").WithArguments(new[] { "clone", $"{repo}" }).ExecuteBufferedAsync();
+
+        // print output of git execution
+
+        // standard/success output
+        Console.WriteLine(@$"
+                    REPONSITORY CLONED SUCCESSFULLY
+                    Time Span: {gitResult.RunTime}
+                    Output:
+                    {gitResult.StandardOutput}
+                ");
+
+        Console.ReadLine();
+    }
+
+    private static void InputValidation(ref String? str, String message)
+    {
+        // checks if input string by user is not empty
+        // if so ask user to provide the requested info again
+        while (str == null || str.Trim().Length == 0)
+        {
+            Console.Write(message);
+            str = Console.ReadLine();
+            str = str?.Trim();
+            Thread.Sleep(500);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# GitCloneAutomator
+
+This is a simple C# console application that helps automate the process of cloning a repository from github. It uses the Cliwrap library in order to execute necessary commands that works on Windows, Linux, and MacOS.
+
+## Installation
+1. Clone repository into directory of choice using  `git clone <https-repo-link>`.
+2. Navigate to project directory.
+3. If using the dotnet CLI, you can run `dotnet build` in the terminal to build the projeect, or you can use Visual Studio IDE. 
+4. You can run the application by running `dotnet run`  with dotnet CLI, click on the run button using Visual Studio IDE, or executing the binary in `./bin/Debug/net7.0/GitCloneAutomator`
